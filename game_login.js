df26e02cc277f0902a56e56eb284e4430b1ce863
EhelloE <161968037+EhelloE@users.noreply.github.com>
2024-03-23 18:08:12 -0400

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,7 @@
+// Create function addUser()
+function addUser() {
+  document.getElementById("player1_name_input").value;
+  document.getElementById("player2_name_input").value;
+  window.location("game_page.html");
+}
+
